c9dad090e78dd90effc6208154ad108e9f0466b8
Helga-Vi <katarina.jorgensen@gmail.com>
2024-04-26 18:50:42 +0200

    AWS env

--- save_json.js
+++ save_json.js
@@ -1,17 +1,23 @@
 const fs = require("fs");
 const path = require("path");
+const AWS = require("aws-sdk");
 
-const save = (favNumber) => {
+// Configure AWS SDK with credentials from environment variables
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION || 'your-default-region' // Replace 'your-default-region' with your actual AWS region
+ });
+ 
+const s3 = new AWS.S3()
+
+const save = async (favNumber) => {
   console.log("saving");
-  fs.writeFile(
-    path.join(__dirname, ".", "number.json"),
-    JSON.stringify(favNumber, null, 2),
-    (error) => {
-      if (error) {
-        throw error;
-      }
-    }
-  );
+  await s3.putObject({
+    Body: JSON.stringify(favNumber, null, 2),
+    Bucket: "your_bucket_name",
+    Key: "number.json",
+  }).promise()
 };
 
 module.exports = { save };
